02a97af3b42525d9ac6e8d84dcec3714ab1ff40e
AaryanSharma <sharmaaaryan195@gmail.com>
2023-07-16 16:23:36 +0530

    Counter App

---
+++ week 1 assignment/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+               'Counter Value:',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(width: 20),
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
